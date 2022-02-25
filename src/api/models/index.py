c639bb8d9e6adc0e94b1456f412525a95fc1c84a
Marina Rojo <marinarojo.cc@gmail.com>
2022-02-25 10:42:48 +0000

    fix: unused code removed

--- src/api/models/index.py
+++ src/api/models/index.py
@@ -2,5 +2,4 @@ from api.models.db import db
 from api.models.user import User
 from api.models.community import Community
 from api.models.rel_user_community import Rel_user_community
-from api.models.incident import Incident
-from api.models.expense import Expense
+
