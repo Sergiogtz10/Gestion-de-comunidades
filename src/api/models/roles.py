33d177b7ab602033934d8cc369689ef11cc085fd
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-02-26 11:42:57 +0000

    borrando migrations y arreglando modelo de roles

--- src/api/models/roles.py
+++ src/api/models/roles.py
@@ -1,12 +1,14 @@
 from api.models.db import db
 
 class Roles(db.Model):
-    role_name = db.Column(db.String(100), primary_key=True, unique=False)
+    role_id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(100), unique=False)
 
     def __repr__(self):
         return '<Roles %r>' % self.role_name
 
     def serialize(self):
         return {
+            "role_id": self.role_id,
             "role_name": self.role_name
         }
--- src/api/models/user.py
+++ src/api/models/user.py
@@ -7,7 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
-    role_name = db.Column(db.String(100), db.ForeignKey('roles.role_name'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.role_id'))
     roles = db.relationship('Roles', backref='user')
 
     def __repr__(self):
@@ -19,17 +19,5 @@ class User(db.Model):
             "first_name": self.first_name,
             "last_name": self.last_name,
             "email": self.email,
-            "role": self.role_name
-        }
-
-
-class Roles(db.Model):
-    role_name = db.Column(db.String(100), primary_key=True, unique=False)
-
-    def __repr__(self):
-        return '<Roles %r>' % self.role_name
-
-    def serialize(self):
-        return {
-            "role_name": self.role_name
+            "role": self.role_id
         }
