1114de470a39e687e51d58e4916485b175cb8d38
Marina Rojo <marinarojo.cc@gmail.com>
2022-03-01 20:04:59 +0000

    feat:rel_user_community backend finished

--- src/seed_data.py
+++ src/seed_data.py
@@ -12,12 +12,12 @@ data = {
 
     "Community":[
         {
-            "id":1,
+            
             "address":"C/ Vilafranca 2, Barcelona",
             "flats":15
         },
         {
-            "id":2,
+            
             "address":"C/ Industria 64, Segovia",
             "flats":34
         }
