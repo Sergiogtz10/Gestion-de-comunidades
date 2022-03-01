b6be9c72c18815dcc184a926effe61834fcc9e15
Marina Rojo <marinarojo.cc@gmail.com>
2022-03-01 20:43:51 +0000

    try and except added on controller

--- src/api/app/rel_user_community/controller.py
+++ src/api/app/rel_user_community/controller.py
@@ -2,8 +2,19 @@ from api.models.index import db, Rel_user_community
 from flask import jsonify
 
 def create_rel(community_id, user_id):
-    new_rel=Rel_user_community(user_id=user_id,community_id=community_id)
-    db.session.add(new_rel)
-    db.session.commit()
-    return new_rel.serialize()
+    try:
+        if community_id is None:
+            return False
+        if user_id is None:
+            return False
+
+        new_rel=Rel_user_community(user_id=user_id,community_id=community_id)
+        db.session.add(new_rel)
+        db.session.commit()
+        return new_rel.serialize()
+
+    except Exception as err:
+        db.session.rollback()
+        print('[ERROR REGISTER USER]: ', err)
+        return None
 
