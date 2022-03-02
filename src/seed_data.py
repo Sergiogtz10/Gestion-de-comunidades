884a7c822cd4cc2462206fc7f1ccb7565ec19bf0
Marina Rojo <46401413+MarinaRojo@users.noreply.github.com>
2022-03-02 08:26:32 +0100

    Backend/rel user community (#3)
    * Seed file changed
    
    * feat: rel_user_community owner registration
    
    * feat:rel_user_community backend finished
    
    * try and except added on controller

---
+++ src/seed_data.py
@@ -0,0 +1,25 @@
+data = {
+    "Roles":[
+        {
+            "role_id": 1,
+            "role_name": "Administrador"
+        },
+        {
+            "role_id": 2,
+            "role_name": "Propietario"
+        }
+    ],
+
+    "Community":[
+        {
+            
+            "address":"C/ Vilafranca 2, Barcelona",
+            "flats":15
+        },
+        {
+            
+            "address":"C/ Industria 64, Segovia",
+            "flats":34
+        }
+    ]
+}
