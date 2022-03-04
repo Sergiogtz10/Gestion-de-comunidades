b61c516f3b6f0cc003e0502e5a556482bd6cba70
Marina Rojo <46401413+MarinaRojo@users.noreply.github.com>
2022-03-04 20:00:14 +0100

    feat: Incidents CRUD (#5)
    * feat: CRUD incidents
    
    * fix: indent error fixed
    
    * refactor: refactoring controller functions
    
    * fix: get incidents route fixed

--- src/api/models/index.py
+++ src/api/models/index.py
@@ -5,4 +5,5 @@ from api.models.user import User
 from api.models.roles import Roles
 from api.models.community import Community
 from api.models.rel_user_community import Rel_user_community
+from api.models.incident import Incident
 
--- src/seed_data.py
+++ src/seed_data.py
@@ -1,3 +1,5 @@
+from api.shared.encrypte_pass import encryp_pass
+
 data = {
     "Roles":[
         {
@@ -12,14 +14,77 @@ data = {
 
     "Community":[
         {
-            
+            "id":1,
             "address":"C/ Vilafranca 2, Barcelona",
             "flats":15
         },
         {
-            
+            "id":2,
             "address":"C/ Industria 64, Segovia",
             "flats":34
         }
-    ]
+    ],
+    "User":[
+        {
+            "id":1,
+            "first_name":"Marina",
+            "last_name":"Rojo",
+            "flat_number":"5ºA",
+            "phone_number":"602602602",
+            "email":"marina@gmail.com",
+            "password": encryp_pass("1234"),
+            "role_id":1
+        },
+          {
+            "id":2,
+            "first_name":"Mireia",
+            "last_name":"Gonzalez",
+            "flat_number":"3ºB",
+            "phone_number":"602602603",
+            "email":"mireia@gmail.com",
+            "password": encryp_pass("1234"),
+            "role_id":2
+        }
+    ],
+
+    "Rel_user_community":[
+        {
+            "user_id":1,
+            "community_id":1
+        },
+
+        {
+            "user_id":2,
+            "community_id":1
+        }
+    ],
+
+    "Incident":[
+    {
+        "bill_id": null,
+        "community_id": 1,
+        "description": "Cristal roto",
+        "id": 1,
+        "severity": "Grave",
+        "user_id": 2,
+        "zone": "Cocina"
+    },
+    {
+        "bill_id": null,
+        "community_id": 1,
+        "description": "Cesped levantado",
+        "id": 2,
+        "severity": "Leve",
+        "user_id": 2,
+        "zone": "Piscina"
+    },
+    {
+        "bill_id": null,
+        "community_id": 1,
+        "description": "No funciona el grifo, no sale agua",
+        "id": 3,
+        "severity": "Medio",
+        "user_id": 1,
+        "zone": "Baño"
+    }
 }
