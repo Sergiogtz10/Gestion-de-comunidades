430402e01836f74533c6028f39c4d618253e2d23
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-03-06 20:04:17 +0000

    fix: responsivness

--- src/front/js/layout/Navbar/Navbar.jsx
+++ src/front/js/layout/Navbar/Navbar.jsx
@@ -1,20 +1,85 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <>
-      <aside className="sidebar m-0 p-0">
-        <nav className="navbar m-0 p-0">
-          <ul className="m-0 p-0">
-            <li className="nav-element w-100">HOME</li>
-            <li className="nav-element">INCIDENCIAS</li>
-            <li className="nav-element">ACTIVIDADES</li>
-            <li className="nav-element">FACTURAS</li>
-            <li className="nav-element">PROVEEDORES</li>
-          </ul>
-        </nav>
-      </aside>
+      <nav className="d-flex align-items-center navbar-mobile">
+        <button
+          className="d-flex btn btn-primary me-2"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvas-navbar"
+          aria-controls="offcanvas-navbar"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            className="bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </button>
+      </nav>
+
+      <div
+        className="offcanvas offcanvas-start"
+        tabindex="-1"
+        id="offcanvas-navbar"
+        aria-labelledby="offcanvasExampleLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+            Offcanvas
+          </h5>
+          <button
+            type="button"
+            className="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body">
+          <div>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text, images, lists, etc.
+          </div>
+          <div className="dropdown mt-3">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-bs-toggle="dropdown"
+            >
+              Dropdown button
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <nav className="sidebar navbar"></nav>
     </>
   );
 };
