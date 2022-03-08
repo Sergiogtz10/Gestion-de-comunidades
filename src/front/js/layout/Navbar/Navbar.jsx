3612a50d8a2590590f1d3b896fa05b38fc6cb3d3
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-03-08 20:30:46 +0000

    feat: more icons

--- src/front/js/layout/Navbar/Navbar.jsx
+++ src/front/js/layout/Navbar/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
             <img id="navbar-desktop-logo" src={temporaryLogo} alt="" />
           </div>
           <ul className="m-0 p-0">
-            <li className="nav-element nav-link m-0 p-0">
+            <li className="nav-element nav-link m-0 p-0 mt-5">
               <Link to="/" className="link-tag">
                 <div className="d-flex align-items-center">
                   <svg
@@ -81,11 +81,50 @@ const Navbar = () => {
             <li className="nav-element nav-link m-0 p-0">INCIDENCIAS</li>
             <li className="nav-element nav-link m-0 p-0">
               <Link to="/actividades" className="link-tag">
-                <p className="m-0">ACTIVIDADES</p>
+                <div className="d-flex align-items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#e9c46a"
+                    className="bi bi-calendar-event-fill navbar-icon mb-1"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
+                  </svg>
+
+                  <p className="m-0 text-navbar-router">ACTIVIDADES</p>
+                </div>
               </Link>
             </li>
-            <li className="nav-element nav-link m-0 p-0">FACTURAS</li>
-            <li className="nav-element nav-link m-0 p-0">PROVEEDORES</li>
+            <li className="nav-element nav-link m-0 p-0">
+              <Link to="/facturas" className="link-tag">
+                <div className="d-flex align-items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#e9c46a"
+                    className="bi bi-bank2 navbar-icon mb-1"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8.277.084a.5.5 0 0 0-.554 0l-7.5 5A.5.5 0 0 0 .5 6h1.875v7H1.5a.5.5 0 0 0 0 1h13a.5.5 0 1 0 0-1h-.875V6H15.5a.5.5 0 0 0 .277-.916l-7.5-5zM12.375 6v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zM8 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2zM.5 15a.5.5 0 0 0 0 1h15a.5.5 0 1 0 0-1H.5z" />
+                  </svg>
+                  <p className="m-0 text-navbar-router">FACTURAS</p>
+                </div>
+              </Link>
+            </li>
+            <li className="nav-element nav-link m-0 p-0">
+              <Link to="/proveedores" className="link-tag">
+                <div className="d-flex align-items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#e9c46a"
+                    className="bi bi-droplet-fill navbar-icon mb-1"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 16a6 6 0 0 0 6-6c0-1.655-1.122-2.904-2.432-4.362C10.254 4.176 8.75 2.503 8 0c0 0-6 5.686-6 10a6 6 0 0 0 6 6ZM6.646 4.646l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448c.82-1.641 1.717-2.753 2.093-3.13Z" />
+                  </svg>
+                  <p className="m-0 text-navbar-router">PROVEEDORES</p>
+                </div>
+              </Link>
+            </li>
           </ul>
         </nav>
       </aside>
