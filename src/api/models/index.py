59ef16bd6a30aada40ffafa1b06e3e8ee1f76d78
Marina Rojo <46401413+MarinaRojo@users.noreply.github.com>
2022-03-10 19:39:05 +0100

    Backend/bill (#7)
    * feat: CRUD incidents
    
    * fix: indent error fixed
    
    * refactor: refactoring controller functions
    
    * fix: get incidents route fixed
    
    * feat: bill routes created
    
    * feat: bill controller and routers ready to check
    
    * fake bills created in seed
    
    * fix seed_data
    
    * bill create and get OK. Working on modify.
    
    * Backend Bill checked
    
    * fix: bill model fixed
    
    * refactor verify_admin
    
    * feat: backend bill finished

--- src/api/models/index.py
+++ src/api/models/index.py
@@ -4,6 +4,7 @@ from api.models.user import User
 from api.models.roles import Roles
 from api.models.community import Community
 from api.models.rel_user_community import Rel_user_community
-from api.models.activities import Activities
 from api.models.incident import Incident
+from api.models.bill import Bill
+from api.models.activities import Activities
 
---
+++ src/api/models/provider.py
@@ -0,0 +1,18 @@
+from api.models.db import db
+
+
+class Provider(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+    
+
+
+    def __repr__(self):
+        return '<Provider %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            
+        }
