83d9f500213d44535b31bfef5c5c33cb79eacc3d
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-03-10 16:22:25 +0000

    feat: incidents dropdown

--- src/front/js/layout/Navbar/Navbar.jsx
+++ src/front/js/layout/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import temporaryLogo from "../../../img/MasterKeyLogo.png";
 
 const Navbar = () => {
+  const [incidentsDropdown, setIncidentsDropdown] = useState(false);
+
+  const handleIncidentsClick = () => {
+    if (!incidentsDropdown) {
+      setIncidentsDropdown(true);
+    } else {
+      setIncidentsDropdown(false);
+    }
+  };
+
+  const handleClick = () => {
+    if (incidentsDropdown) {
+      setIncidentsDropdown(false);
+    }
+  };
+
+  console.log({ incidentsDropdown });
   return (
     <>
       {/* NAVBAR MOBILE ONLY */}
@@ -64,7 +81,10 @@ const Navbar = () => {
           <ul className="m-0 p-0">
             <li className="nav-element nav-link m-0 p-0 mt-5">
               <Link to="/" className="link-tag">
-                <div className="d-flex align-items-center">
+                <div
+                  className="d-flex align-items-center"
+                  onClick={handleClick}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="#e9c46a"
@@ -78,10 +98,37 @@ const Navbar = () => {
               </Link>
             </li>
 
-            <li className="nav-element nav-link m-0 p-0">INCIDENCIAS</li>
+            <li className="nav-element nav-link m-0 p-0">
+              <div
+                className="d-flex align-items-center"
+                id="dropdown-incidencias"
+                onClick={handleIncidentsClick}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#e9c46a"
+                  className="bi bi-exclamation-triangle-fill navbar-icon mb-1"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg>
+                <p className="text-navbar-router m-0">INCIDENCIAS</p>
+              </div>
+            </li>
+
+            {incidentsDropdown ? (
+              <>
+                <li className="nav-element nav-link m-0 p-0">PARTICULARES</li>
+                <li className="nav-element nav-link m-0 p-0">COMUNES</li>
+              </>
+            ) : null}
+
             <li className="nav-element nav-link m-0 p-0">
               <Link to="/actividades" className="link-tag">
-                <div className="d-flex align-items-center">
+                <div
+                  className="d-flex align-items-center"
+                  onClick={handleClick}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="#e9c46a"
@@ -97,7 +144,10 @@ const Navbar = () => {
             </li>
             <li className="nav-element nav-link m-0 p-0">
               <Link to="/facturas" className="link-tag">
-                <div className="d-flex align-items-center">
+                <div
+                  className="d-flex align-items-center"
+                  onClick={handleClick}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="#e9c46a"
@@ -112,7 +162,10 @@ const Navbar = () => {
             </li>
             <li className="nav-element nav-link m-0 p-0">
               <Link to="/proveedores" className="link-tag">
-                <div className="d-flex align-items-center">
+                <div
+                  className="d-flex align-items-center"
+                  onClick={handleClick}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="#e9c46a"
