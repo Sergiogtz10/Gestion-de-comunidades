d57ff0a6b22bc3d7a4fde563794d6fc8936c36b6
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-03-11 21:21:50 +0000

    refactor: changed navbar links to a component

--- src/front/js/layout/Navbar/Navbar.jsx
+++ src/front/js/layout/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
+
+//images
 import logo from "../../../img/MasterKeyLogo.png";
+
+//components
+import NavbarLinkDesktop from "../../component/NavbarLinkDesktop/NavbarLinkDesktop.jsx";
 
 const Navbar = () => {
   const [incidentsDropdown, setIncidentsDropdown] = useState(false);
@@ -80,27 +85,27 @@ const Navbar = () => {
             <img id="navbar-desktop-logo" src={logo} alt="" />
           </div>
           <ul className="m-0 p-0">
-            <li className="nav-element nav-link m-0 p-0 mt-4">
-              <Link to="/" className="link-tag">
-                <div
-                  className="d-flex align-items-center"
-                  onClick={handleClick}
+            <NavbarLinkDesktop
+              individualClassLi="mt-4"
+              link="/"
+              clickFunction={handleClick}
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#e9c46a"
+                  className="bi bi-house-door-fill navbar-icon mb-1"
+                  viewBox="0 0 16 16"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#e9c46a"
-                    className="bi bi-house-door-fill navbar-icon mb-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
-                  </svg>
-                  <p className="text-navbar-router m-0">HOME</p>
-                </div>
-              </Link>
-            </li>
+                  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
+                </svg>
+              }
+              pClassName="text-navbar-router"
+              linkName="HOME"
+            />
             <div className="d-flex justify-content-center">
               <div className="separation"></div>
             </div>
+
             <li className="nav-element nav-link m-0 p-0">
               <div
                 className="d-flex align-items-center"
@@ -121,111 +126,99 @@ const Navbar = () => {
 
             {incidentsDropdown ? (
               <>
-                <li className="nav-element nav-link m-0 p-0 dropdown-incidents-li">
-                  <Link to="/incidencias/particulares" className="link-tag">
-                    <div className="d-flex align-items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="#e9c46a"
-                        className="bi bi-person-fill navbar-icon mb-1 ps-3"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                      </svg>
-                      <p className="text-navbar-router-dropdown m-0">
-                        Particulares
-                      </p>
-                    </div>
-                  </Link>
-                </li>
-                <li className="nav-element nav-link m-0 p-0 dropdown-incidents-li">
-                  <Link to="/incidencias/comunidad" className="link-tag">
-                    <div className="d-flex align-items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="#e9c46a"
-                        className="bi bi-people-fill navbar-icon mb-1 ps-3"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                        <path
-                          fill-rule="evenodd"
-                          d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"
-                        />
-                        <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
-                      </svg>
-                      <p className="text-navbar-router-dropdown m-0">
-                        Comunidad
-                      </p>
-                    </div>
-                  </Link>
-                </li>
+                <NavbarLinkDesktop
+                  individualClassLi="dropdown-incidents-li"
+                  link="/incidencias/particulares"
+                  icon={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="#e9c46a"
+                      className="bi bi-person-fill navbar-icon mb-1 ps-3"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                    </svg>
+                  }
+                  pClassName="text-navbar-router-dropdown"
+                  linkName="Particulares"
+                />
+                <NavbarLinkDesktop
+                  individualClassLi="dropdown-incidents-li"
+                  link="/incidencias/comunidad"
+                  icon={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="#e9c46a"
+                      className="bi bi-people-fill navbar-icon mb-1 ps-3"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                      <path d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z" />
+                      <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
+                    </svg>
+                  }
+                  pClassName="text-navbar-router-dropdown"
+                  linkName="Comunidad"
+                />
               </>
             ) : null}
             <div className="d-flex justify-content-center">
               <div className="separation"></div>
             </div>
-            <li className="nav-element nav-link m-0 p-0">
-              <Link to="/actividades" className="link-tag">
-                <div
-                  className="d-flex align-items-center"
-                  onClick={handleClick}
+            <NavbarLinkDesktop
+              link="/actividades"
+              clickFunction={handleClick}
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#e9c46a"
+                  className="bi bi-calendar-event-fill navbar-icon mb-1"
+                  viewBox="0 0 16 16"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#e9c46a"
-                    className="bi bi-calendar-event-fill navbar-icon mb-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
-                  </svg>
-
-                  <p className="m-0 text-navbar-router">ACTIVIDADES</p>
-                </div>
-              </Link>
-            </li>
+                  <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
+                </svg>
+              }
+              pClassName="text-navbar-router"
+              linkName="ACTIVIDADES"
+            />
             <div className="d-flex justify-content-center">
               <div className="separation"></div>
             </div>
-            <li className="nav-element nav-link m-0 p-0">
-              <Link to="/facturas" className="link-tag">
-                <div
-                  className="d-flex align-items-center"
-                  onClick={handleClick}
+            <NavbarLinkDesktop
+              link="/facturas"
+              clickFunction={handleClick}
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#e9c46a"
+                  className="bi bi-bank2 navbar-icon mb-1"
+                  viewBox="0 0 16 16"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#e9c46a"
-                    className="bi bi-bank2 navbar-icon mb-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8.277.084a.5.5 0 0 0-.554 0l-7.5 5A.5.5 0 0 0 .5 6h1.875v7H1.5a.5.5 0 0 0 0 1h13a.5.5 0 1 0 0-1h-.875V6H15.5a.5.5 0 0 0 .277-.916l-7.5-5zM12.375 6v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zM8 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2zM.5 15a.5.5 0 0 0 0 1h15a.5.5 0 1 0 0-1H.5z" />
-                  </svg>
-                  <p className="m-0 text-navbar-router">FACTURAS</p>
-                </div>
-              </Link>
-            </li>
+                  <path d="M8.277.084a.5.5 0 0 0-.554 0l-7.5 5A.5.5 0 0 0 .5 6h1.875v7H1.5a.5.5 0 0 0 0 1h13a.5.5 0 1 0 0-1h-.875V6H15.5a.5.5 0 0 0 .277-.916l-7.5-5zM12.375 6v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zM8 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2zM.5 15a.5.5 0 0 0 0 1h15a.5.5 0 1 0 0-1H.5z" />
+                </svg>
+              }
+              pClassName="text-navbar-router"
+              linkName="FACTURAS"
+            />
             <div className="d-flex justify-content-center">
               <div className="separation"></div>
             </div>
-            <li className="nav-element nav-link m-0 p-0">
-              <Link to="/proveedores" className="link-tag">
-                <div
-                  className="d-flex align-items-center"
-                  onClick={handleClick}
+            <NavbarLinkDesktop
+              link="/proveedores"
+              clickFunction={handleClick}
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#e9c46a"
+                  className="bi bi-droplet-fill navbar-icon mb-1"
+                  viewBox="0 0 16 16"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#e9c46a"
-                    className="bi bi-droplet-fill navbar-icon mb-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 16a6 6 0 0 0 6-6c0-1.655-1.122-2.904-2.432-4.362C10.254 4.176 8.75 2.503 8 0c0 0-6 5.686-6 10a6 6 0 0 0 6 6ZM6.646 4.646l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448c.82-1.641 1.717-2.753 2.093-3.13Z" />
-                  </svg>
-                  <p className="m-0 text-navbar-router">PROVEEDORES</p>
-                </div>
-              </Link>
-            </li>
+                  <path d="M8 16a6 6 0 0 0 6-6c0-1.655-1.122-2.904-2.432-4.362C10.254 4.176 8.75 2.503 8 0c0 0-6 5.686-6 10a6 6 0 0 0 6 6ZM6.646 4.646l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448c.82-1.641 1.717-2.753 2.093-3.13Z" />
+                </svg>
+              }
+              pClassName="text-navbar-router"
+              linkName="PROVEEDORES"
+            />
           </ul>
         </nav>
 
@@ -239,10 +232,7 @@ const Navbar = () => {
               id="avatar-icon"
             >
               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-              <path
-                fill-rule="evenodd"
-                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-              />
+              <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
             </svg>
           </button>
           <div id="avatar-menu">
