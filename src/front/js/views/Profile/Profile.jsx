b6c458753cfdaaa24645d86596722b3f8d2e7b99
Sergiogtz10 <sergiogutierrezcn@gmail.com>
2022-03-17 23:05:46 +0000

    avances visuales en profile

--- src/front/js/views/Profile/Profile.jsx
+++ src/front/js/views/Profile/Profile.jsx
@@ -10,6 +10,8 @@ const Profile = () => {
     last_name: "",
     email: "",
     phone_number: "",
+    password: "",
+  
   });
   
     const getdata = async () => {
@@ -21,7 +23,9 @@ const Profile = () => {
         first_name: data.first_name,
         last_name: data.last_name,
         email: data.email,
-        phone_number: data.phone_number,  
+        phone_number: data.phone_number, 
+        password: data.password,
+         
       }
       setDataUser(newUser);
       console.log(newUser)
@@ -36,26 +40,40 @@ const Profile = () => {
 
   return (
     <div>
-      <h3 className="container fluid">{dataUser.first_name}</h3>
-      <div id="card" className="container fluid card p-4" style={{width: "700px"}} >
-      <h4 id="titlecardprofile">Datos personales</h4>
+      <h3 id="title" className="container mt-5" style={{width: "500px"}}>{dataUser.first_name + " "+ dataUser.last_name}</h3>
+      <div id="card" className="container card p-4" style={{width: "500px"}} >
+      <h4 id="titlecardprofile" className="mb-4 text-center">Datos personales</h4>
         <div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">{dataUser.first_name}</li>
-              <li className="list-group-item">{dataUser.last_name}</li>
-              <li className="list-group-item">{dataUser.email}</li>
-              <li className="list-group-item">{dataUser.phone_number}</li>
-            </ul>
-            <div className="p-1">
-              <button type="submit" className="btn btn-primary" id="boton">
-                Actualizar
-              </button>
-            </div>
+          <form className="text-center">
+              <div className="mb-3">
+                <input type="text" defaultValue={dataUser.first_name}  id="inputprofile"></input>
+              </div>
+              <div className="mb-3">
+                <input type="text" defaultValue={dataUser.last_name}  id="inputprofile"></input>
+              </div>
+              <div className="mb-3">
+                <input type="text" defaultValue={dataUser.email}  id="inputprofile"></input>
+              </div>
+              <div className="mb-3">
+                <input type="text" defaultValue={dataUser.phone_number}  id="inputprofile"></input>
+              </div>
+              <div className="mb-3">
+                <input type="text" placeholder="Contraseña" defaultValue={dataUser.password}  id="inputprofile"></input>
+              </div>
+              <div className="mb-3">
+                <input type="text" placeholder="Repetir contraseña" id="inputprofile"></input>
+              </div>
+              <div className="p-1">
+                <button type="submit" className="btn btn-primary" id="boton">
+                  Actualizar
+                </button>
+              </div>
+            </form>
           </div>
         </div>
         <div className="p-4">
-          <div id="card" className="container fluid card p-4" style={{width: "700px"}}>
-              <h4 id="titlecardprofile">Comunidades</h4>
+          <div id="card" className="container text-center card p-4" style={{width: "500px"}}>
+              <h4 id="titlecardprofile" className=" text-center mb-4">Comunidades</h4>
              
               <div className="d-flex flex-column col-md-5 gap-3 p-3">
                 <Link to="/form/community">
