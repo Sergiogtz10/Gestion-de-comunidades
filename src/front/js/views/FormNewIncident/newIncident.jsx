456834392d398904ed3512df617294c64a42c4c7
Marina Rojo <marinarojo.cc@gmail.com>
2022-03-17 13:15:38 +0000

    new incident form view created

--- src/front/js/views/FormNewIncident/newIncident.jsx
+++ src/front/js/views/FormNewIncident/newIncident.jsx
@@ -1,12 +1,52 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../../store/appContext";
+import "./newIncident.css";
 
 const FormNewIncident = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="p-5">
-      <h1>Formulario nueva incidencia</h1>
+    <div className="p-5 ">
+      <h4 id="title" className="text-center font-weight-bold">
+        Nueva Incidencia
+      </h4>
+      <hr className="my-3"></hr>
+      <form className="card p-5">
+        <div className="py-3">
+          <label className="form-label">Descripción</label>
+          <input
+            type="descripcion"
+            className="form-control shadow-sm"
+            placeholder="Descripción"
+            name="descripcion"
+          ></input>
+        </div>
+
+        <div className="py-3">
+          <label className="form-label">Zona</label>
+          <input
+            type="zona"
+            className="form-control shadow-sm"
+            placeholder="Zona"
+            name="zona"
+          ></input>
+        </div>
+
+        <div className="py-3">
+          <label className="form-label">Gravedad</label>
+          <select className="form-select" defaultValue={"Leve"}>
+            <option value="Leve">Leve</option>
+            <option value="Media">Media</option>
+            <option value="Grave">Grave</option>
+          </select>
+        </div>
+
+        <div className="py-3">
+          <button type="submit" className="btn btn-primary" id="boton">
+            Crear
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
