f4655dcd1c668b35035b634050eb528312240e8b
Sergiogtz10 <sergiogutierrezcn@gmail.com>
2022-03-19 19:41:36 +0000

    avances en Landing

--- src/front/js/Layout/layout.js
+++ src/front/js/Layout/layout.js
@@ -52,7 +52,6 @@ const Layout = () => {
               <h1>Not found!</h1>
             </Route>
           </Switch>
-          <Footer />
         </ScrollToTop>
       </BrowserRouter>
     </div>
