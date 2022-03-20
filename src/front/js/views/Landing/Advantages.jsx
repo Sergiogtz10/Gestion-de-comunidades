a4ede63281d53f08d460df7cf5cd53b71901a197
Sergiogtz10 <sergiogutierrezcn@gmail.com>
2022-03-20 14:52:54 +0000

    avances en Landing 20-03

--- src/front/js/views/Landing/Advantages.jsx
+++ src/front/js/views/Landing/Advantages.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Advantages = () =>{
     return (
         <div>
--- src/front/js/views/Landing/Body.jsx
+++ src/front/js/views/Landing/Body.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Body.css";
 
 const Body = () =>{
     return(
-        <div id="back" className="container-fluid">
-            <div className="container py-5">
-                <div className="d-flex flex-column flex-lg-row align-items-center mx-5 ">
-                    <div className="col-md-8 ps-3 ps-lg-5 p-5">
-                        <h1>Master Key</h1>
-                        <p className="text-muted fs-5">Master Key es la aplicación que hará la relación con tu comunidad de vecinos más sencilla y agradable</p>
-                        <div className="d-flex flex-column col-md-5 gap-4 justify-content-center">
-                        <Link to="/form/admin">
-                            <button type="button" className="btn btn-success">Registrarse como administrador</button>
-                        </Link>
-                        <Link to="/Login">
-                        <button   type="button" className="btn btn-success">Log In</button>
-                        </Link>
+        <div>
+            <div>
+            <div id="backgroundbody" className="container-fluid row">
+                <div id="divtitle" className="container align-items-center text-center" style={{width: "400px"}}>
+                        <div className="background-white">
+                            <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Indie+Flower&family=Montserrat:wght@300&family=Pacifico&family=Righteous&display=swap" rel="stylesheet"></link>
+                            <h2 id="titlebody" className="header-text-one mb-3">MasterKey es la aplicación que hará la relación con tu comunidad de vecinos más agradable y sencilla</h2>
                         </div>
-                    </div>
-                    <img src="https://perezparras.com/wp-content/uploads/2018/09/Neighbours-in-building-434KB-790x790.png" alt="" className="rounded-circle img-fluid col-md-3  ms-lg-2"></img>
                 </div>
+                <div id="login">
+                    <div className="d-flex flex-column text-center row">
+
+                        <h5 id="titlelogin">INICIA SESIÓN O REGÍSTRATE</h5>
+                        <Link to="/Login">
+                            <button   type="button" className="btn btn-success text-center mt-4 col-md-10" id="boton">Iniciar sesión</button>
+                        </Link>
+                        <Link to="/form/admin">
+                            <button type="button" className="btn btn-success mt-4 col-md-10" id="boton">Registrarse</button>
+                        </Link>
+                        <p id="textregister" className="mt-3">SI TODAVÍA NO TIENES UNA CUENTA DE USUARIO DE MASTERKEY UTILIZA ESTA OPCIÓN PARA ACCEDER AL FORMULARIO DE REGISTRO. TE SOLICITAREMOS LA INFORMACIÓN IMPRESCINDIBLE PARA UTILIZAR LA APLICACIÓN</p>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     )
