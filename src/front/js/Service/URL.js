9c4a28809a2291ff8b8b8f96f6cd0b8165ee8664
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-03-23 19:06:22 +0000

    starting home page view

--- src/front/js/Service/URL.js
+++ src/front/js/Service/URL.js
@@ -1,5 +1,5 @@
 export const URL =
-  "https://3001-sergiogtz10-gestiondecom-mnsq6n27r4k.ws-eu38.gitpod.io";
+  "https://3001-sergiogtz10-gestiondecom-lwxwoscfg98.ws-eu38.gitpod.io";
 
 export const getToken = () => {
   return localStorage.getItem("token");
--- src/front/js/views/HomePage/HomePage.jsx
+++ src/front/js/views/HomePage/HomePage.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { Context } from "../../store/appContext";
+import { Link } from "react-router-dom";
+
+import "./home-page.css";
 
 const HomePage = () => {
+  const { store, actions } = useContext(Context);
+
+  useEffect(() => {
+    actions.getCommunity();
+    actions.getIncidents();
+  }, []);
+
   return (
-    <div className="content">
-      <h1>HOME PAGE</h1>
+    <div className="container-fluid content mt-5">
+      <h1>Home</h1>
+      <h5 className="table-name">ÚLTIMAS INCIDENCIAS</h5>
+      <div className="div-table p-2">
+        <div className="table-responsive">
+          <table className="table table-light table-striped m-0">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Zona</th>
+                <th scope="col">Gravedad</th>
+                <th scope="col">Estado</th>
+              </tr>
+            </thead>
+            <tbody>
+              {store.incidents
+                .filter(
+                  (incidents) => incidents.community_id == store.community
+                )
+                .map((incident, index) => {
+                  let count = 0;
+
+                  while (count <= 3) {
+                    count++;
+                    return (
+                      <tr key={index}>
+                        <th scope="col">{index + 1}</th>
+                        <td scope="col">{incident.description}</td>
+                        <td scope="col">{incident.zone}</td>
+                        <td scope="col">{incident.severity}</td>
+                        <td scope="col">{incident.status}</td>
+                      </tr>
+                    );
+                  }
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
