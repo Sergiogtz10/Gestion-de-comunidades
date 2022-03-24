51cc199ec468cf1b13c09fd1500064723f24727c
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-03-24 19:26:07 +0000

    últimas incidencias de la comunidad finsished

--- src/front/js/views/HomePage/HomePage.jsx
+++ src/front/js/views/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Context } from "../../store/appContext";
 import { Link } from "react-router-dom";
 
@@ -13,45 +13,56 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="container-fluid content mt-5">
+    <div className="container-fluid content mt-5 me-2">
       <h1>Home</h1>
-      <h5 className="table-name">ÚLTIMAS INCIDENCIAS</h5>
-      <div className="div-table p-2">
-        <div className="table-responsive">
-          <table className="table table-light table-striped m-0">
-            <thead className="thead-dark">
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Descripción</th>
-                <th scope="col">Zona</th>
-                <th scope="col">Gravedad</th>
-                <th scope="col">Estado</th>
-              </tr>
-            </thead>
-            <tbody>
-              {store.incidents
-                .filter(
-                  (incidents) => incidents.community_id == store.community
-                )
-                .map((incident, index) => {
-                  let count = 0;
+      <div className="table-name d-flex justify-content-center mt-5">
+        ÚLTIMAS INCIDENCIAS DE LA COMUNIDAD
+      </div>
 
-                  while (count <= 3) {
-                    count++;
+      <div className="d-flex justify-content-center">
+        <div id="incidents-table" className="div-table m-0">
+          <div className="table-responsive d-flex justify-content-center">
+            <table className="table table-light table-striped m-0 table-homePage">
+              {store.incidents.filter(
+                (incidents) => incidents.community_id == store.community
+              ).length > 0 ? (
+                <tbody>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Descripción</th>
+                    <th scope="col">Zona</th>
+                    <th scope="col">Gravedad</th>
+                    <th scope="col">Estado</th>
+                  </tr>
+                  {store.incidents.slice(0, 3).map((incident, index) => {
                     return (
                       <tr key={index}>
-                        <th scope="col">{index + 1}</th>
-                        <td scope="col">{incident.description}</td>
-                        <td scope="col">{incident.zone}</td>
-                        <td scope="col">{incident.severity}</td>
-                        <td scope="col">{incident.status}</td>
+                        <th scope="row">{index + 1}</th>
+                        <td>{incident.description}</td>
+                        <td>{incident.zone}</td>
+                        <td>{incident.severity}</td>
+                        <td>{incident.status}</td>
                       </tr>
                     );
-                  }
-                })}
-            </tbody>
-          </table>
+                  })}
+                </tbody>
+              ) : (
+                <tbody id="no-incidents-body">
+                  <tr>
+                    <td scope="row">
+                      <strong>¡NO HAY INCIDENCIAS!</strong>
+                    </td>
+                  </tr>
+                </tbody>
+              )}
+            </table>
+          </div>
         </div>
+      </div>
+      <div className="d-flex justify-content-end">
+        <Link to="/incidencias/comunidad">
+          <button className="btn boton-ver-mas-homePage">Ver Más</button>
+        </Link>
       </div>
     </div>
   );
