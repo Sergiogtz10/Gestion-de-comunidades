8c188934427b4b4ee91b499c5bed2f039a3f7e0a
Marina Rojo <marinarojo.cc@gmail.com>
2022-03-26 11:46:04 +0000

    URL fixed

--- src/front/js/Service/URL.js
+++ src/front/js/Service/URL.js
@@ -1,9 +1,5 @@
 export const URL =
-<<<<<<< HEAD
   "https://3001-sergiogtz10-gestiondecom-ftskqhy5cua.ws-eu38.gitpod.io";
-=======
-  "https://3001-sergiogtz10-gestiondecom-mnsq6n27r4k.ws-eu38.gitpod.io";
->>>>>>> cfa5f06c4e4ef2fcbe10936cb15a42c676b6f7a9
 
 export const getToken = () => {
   return localStorage.getItem("token");
