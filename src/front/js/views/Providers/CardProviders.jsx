a0bc45679a8ac2b4cd0d5bc3ec464e98f76193b7
Sergiogtz10 <sergiogutierrezcn@gmail.com>
2022-03-28 23:38:06 +0000

    changes

--- src/front/js/views/Providers/CardProviders.jsx
+++ src/front/js/views/Providers/CardProviders.jsx
@@ -1,21 +1,62 @@
 import React from "react";
-
-
-
-
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../store/appContext.js";
+import "./CardProviders.css"
+import { useParams } from "react-router-dom";
 
 const CardProviders = () =>{
+    const { store, actions } = useContext(Context);
+    const { community_id } = useParams();
 
+    useEffect(() => {
+        actions.getUser();
+        actions.getProviders(community_id);
+      }, []);
  return (
-     <div>
-        <div className="card mb-3 mt-4" id="CardProvider">
-             <div className="card-header"></div>
-                <div className="card-body">
-                    <h5 className="card-title"></h5> 
-                </div>
+    <div className="container-fluid m-auto mt-5 content">
+    <h1>Proveedores</h1>
+    <div className="navbar row">
+      {store.role.role_id == 1 ? (
+        <div>
+          <Link
+            className="btn btn-secondary col-2 mx-4 add"
+            to="/form/provider"
+          >
+            Añadir proveedor
+          </Link>
         </div>
-         
-     </div>)
+      ) : (
+        null
+      )}
+    </div>
+    <table className="table">
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Servicio</th>
+        </tr>
+      </thead>
+      <tbody>
+        {store.providers
+          .filter((provider) => provider.community_id == store.community)
+          .map((prov, index) => {
+            return (
+            <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>
+                    <input defaultValue={prov.name} className="form-control"></input>
+                </td>
+                <td>
+                    <input defaultValue={prov.service} className="form-control"></input>
+                </td> 
+            </tr>
+            );})}
+      </tbody>
+      </table>
+ </div>
+ )
  
 };
 
--- src/front/js/views/Providers/Providers.jsx
+++ src/front/js/views/Providers/Providers.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import { useContext, useEffect} from "react";
-import { Context } from "../../store/appContext.js";
 import "./Providers.css"
 import CardProviders from "./CardProviders.jsx";
-import { Link } from "react-router-dom";
+
 
 const Providers = () =>{
-    const { store, actions } = useContext(Context);
-    useEffect(() => {
-        actions.getUser();
-      }, []);
 
  return (
      <div className="container" id="divProvider">
          <div className="container-fluid mt-5 content row">
-            <h1 className="col-sm-12" id="titleproviders">Proveedores de la comunidad</h1>
-            {store.role.role_id == 1 ? (
-            <Link className="btn btn-secondary col-2 mx-4 add" to="/form/provider"> Añadir proveedor </Link>) : null}
             <CardProviders />
          </div>
          
