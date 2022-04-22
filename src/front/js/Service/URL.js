432c2248d6b30253c36db9bc7f61f721681d8b0c
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-04-22 13:33:14 +0000

    style(landing footer): text centered

--- src/front/js/Service/URL.js
+++ src/front/js/Service/URL.js
@@ -1,5 +1,5 @@
 export const URL =
-  "https://3001-sergiogtz10-gestiondecom-fu9zhg1mlun.ws-eu38.gitpod.io";
+  "https://3001-sergiogtz10-gestiondeco-px9n1n96dih.ws-eu41.gitpod.io";
 
 export const getToken = () => {
   return localStorage.getItem("token");
